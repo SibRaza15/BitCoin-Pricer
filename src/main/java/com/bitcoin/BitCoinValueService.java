502319ae70a4499c237b3431fe84a3d0b26743ed
Sib Raza <sibraza15@gmail.com>
2018-04-29 22:20:17 -0400

    Updated several files

--- src/main/java/com/bitcoin/BitCoinValueService.java
+++ src/main/java/com/bitcoin/BitCoinValueService.java
@@ -7,7 +7,7 @@ import java.net.URLConnection;
 
 public class BitCoinValueService {
 
-	public static double findPrice() {
+	public  double findPrice() {
 		
 		double result = 0; 
 		try{
@@ -52,3 +52,124 @@ public class BitCoinValueService {
 	}
 		
 	}
+
+
+
+
+
+
+/*
+
+package com.bitcoin;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import org.testng.annotations.DataProvider;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import junitparams.JUnitParamsRunner;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import org.mockito.Mockito;
+
+
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+
+@RunWith(JUnitParamsRunner.class)
+public class BitcoinPricerTest {
+	
+	private BitCoinPricer bcp; 
+	
+	@Mock
+	private BitCoinValueService mockService; 
+	
+	@Before
+	public void beforeMethod() {
+		mockService = mock(BitCoinValueService.class);
+	}
+	
+	@After
+	public void afterMethod() {
+		mockService = null; 
+	}
+	
+
+	@DataProvider
+	public List<Object[]> dp(){
+		final List<Object[]> result = new ArrayList<>();
+	    result.add( new Object[] {6000.00} );
+	    return result; 
+	}
+	
+	@Test 
+	@UseDataProvider("dp")
+	public void testMock (double expectedVal) throws Exception{
+	
+		when(mockService.findPrice()).thenReturn(expectedVal);
+		
+		// Instantiate the object
+		bcp = new BitCoinPricer (mockService);
+		
+		
+		//Test begins 
+		bcp.convertEuro(0);
+		
+		//checking condition
+		assertEquals(bcp.convertEuro(expectedVal), 6000.00,0.1);
+		
+		verify(mockService, times(1)).findPrice();
+	}
+	
+
+	
+	//at the time of testing the euro to dollar conversion rate was 1.227481 and 
+	//Bitcoin price in euros was 7,257.08.
+	
+	/*
+	@Test
+	public void AssertEquals_Expected_Output() {
+		assertEquals(8955.56, BitCoinPricer.convertEuro(service.findPrice()), 0.10);
+	}
+
+	@Test
+	public void AssertNull_BitCoinPricer_Output() {
+		assertNull(null, BitCoinPricer.convertEuro(service.findPrice()));
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void AssertEquals_Null_Ex() {
+		assertEquals(8955.56, BitCoinPricer.convertEuro((Double) null), 0.10);
+	}
+
+	// integration testing
+	@Test
+	public void testAssertEqualsSameObjects() {
+		bitCoinPricer.convertEuro(bitCoinValueService.findPrice());
+		assertEquals("The same object", bitCoinValueService.findPrice(),this.bitCoinValueService.findPrice(), 5.00);
+	}
+	
+	@Test
+	public void testAssertEqualsFalseDiffObject() {
+		BitCoinValueService s = new BitCoinValueService();
+		BitCoinPricer b = new BitCoinPricer(bitCoinValueService);
+		s.findPrice();
+		b.convertEuro(s.findPrice());
+		assertEquals("The object is not the same",s.findPrice(),b.convertEuro(s.findPrice()),5.00);
+	}
+	*/
--- src/main/java/com/bitcoin/main.java
+++ src/main/java/com/bitcoin/main.java
@@ -8,7 +8,7 @@ public class main {
 		BitCoinValueService t = new BitCoinValueService();
 		BitCoinPricer b = new BitCoinPricer(t);
 		
-		double  result = b.convertEuro(t.findPrice());
+		double  result = b.convertEuro();
 		
 		System.out.println("The value of bitcoin in dolllars is "+ "$"+ result);
 
